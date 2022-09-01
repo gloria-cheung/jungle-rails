fe25dbfa8bbf4e70e3c587c47d9b8e8cf2ce3ab3
Gloria <gloriacheung812@gmail.com>
2022-09-01 21:40:07 +0000

    add logout path instead of through /sessions/:id

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,8 @@ Rails.application.routes.draw do
 
   resources :about, only: [:index]
   resources :users, only: [:create, :new]
-  resources :sessions, only: [:create, :new, :destroy]
+  resources :sessions, only: [:create, :new]
+  get '/logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
