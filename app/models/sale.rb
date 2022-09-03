e56e55e2b6a3372457bd05e54a3e299ca09696c3
Gloria <gloriacheung812@gmail.com>
2022-09-03 16:48:36 +0000

    move conditional logic to method in model for whether active, upcoming or finished

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,13 @@
 class Sale < ApplicationRecord
+  def finished?
+    ends_on < Date.current
+  end
+
+  def upcoming?
+    starts_on > Date.current
+  end
+
+  def active?
+    starts_on < Date.current && ends_on > Date.current
+  end
 end
