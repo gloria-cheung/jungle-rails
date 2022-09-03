26317ebe84c40ccd3fad1c6521da41a423496216
Gloria <gloriacheung812@gmail.com>
2022-09-03 18:27:22 +0000

    define method for new and create when these routes are hit

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -1,8 +1,32 @@
 class Admin::SalesController < ApplicationController
+  http_basic_authenticate_with name: ENV["AUTHENTICATION_USERNAME"], password: ENV["AUTHENTICATION_PASSWORD"]
   def index
     @sales = Sale.all.order(starts_on: :desc)
   end
 
   def new
+    @sale = Sale.new
   end
+
+  def create
+    @sale = Sale.new(product_params)
+
+    if @sale.save
+      redirect_to [:admin, :sales], notice: 'Sale created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:sale).permit(
+      :name,
+      :starts_on,
+      :ends_on,
+      :percent_off
+    )
+  end
+
 end
