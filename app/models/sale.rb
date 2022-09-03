90318c2273a02cdcdd396a8fe2c4453b9f1da6fc
Gloria <gloriacheung812@gmail.com>
2022-09-03 17:57:10 +0000

    scope to sale model and add method active

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,10 @@
 class Sale < ApplicationRecord
+  
+  # AR Scope
+  def self.active
+    where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current)
+  end
+
   def finished?
     ends_on < Date.current
   end
