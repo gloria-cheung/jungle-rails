4fcbee58ee1a43c25b89a06d250934cce2b9cc72
Gloria <gloriacheung812@gmail.com>
2022-09-03 18:28:33 +0000

    add validation

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -16,4 +16,9 @@ class Sale < ApplicationRecord
   def active?
     starts_on < Date.current && ends_on > Date.current
   end
+
+  validates :name, presence: true
+  validates :starts_on, presence: true
+  validates :ends_on, presence: true
+  validates :percent_off, presence: true
 end
